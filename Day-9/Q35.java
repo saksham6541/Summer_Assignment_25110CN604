a9f795d0bdecf075591536a967b37f55e9a882f6
saksham6541 <saksham.kaushik@gmail.com>
2026-06-05 01:57:40 +0530

    Day 9 Uploaded

---
+++ Day-9/Q35.java
@@ -0,0 +1,12 @@
+public class Q35 {
+    public static void main(String[] args) {
+        for(char i ='A';i<='E';i++)
+        {
+            for(char j='A';j<=i;j++)
+            {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
